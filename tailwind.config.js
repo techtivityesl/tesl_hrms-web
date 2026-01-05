37be08221ad81ce88b15919ac6bb2b0754a31bb2
techtivityesl <naresh@techtivity.co.in>
2026-01-05 11:08:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,20 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF50",
+        secondary: "#6FCF97",
+        darktext: "#2F3437",
+        muted: "#DADDDD",
+        offwhite: "#F7F7F5"
+      }
+    }
+  },
+  plugins: []
+}
